79bc01da07678932906c0a22cf45c73eb278096e
hong le <vuanhde@yahoo.de>
2026-01-23 04:01:31 +0100

    4f.commit

--- src/main/resources/db/migration/V4__Add_initial_product_data.sql
+++ src/main/resources/db/migration/V4__Add_initial_product_data.sql
@@ -11,4 +11,4 @@ VALUES
     ('510a0d7e-8e83-4193-b483-e27e09ddc34d', 'JavaScript for Gurus', 'book for Gurus', 'Hong Le', 'javascript for gurus', '/images/Antifragile.jpg', 19.99, 'a1b9b31d-e73c-4112-af7c-b68530f38222'),
     ('03fef6ac-1896-4ce8-bd69-b798f85c6e0b', 'Fundamentals of Java', 'book for Beginner', 'Tommy Le', 'fundamentals of java', '/images/Sapiens.jpeg', 39.99, '819e1fbf-8b7e-4f6d-811f-693534916a8b'),
     ('d3588630-ad8e-49df-bbd7-3167f7efb246', 'Spring in Action', 'book for Java Dev', 'Maxell Dam', 'spring in action', '/images/Hackers.jpg', 49.99, '819e1fbf-8b7e-4f6d-811f-693534916a8b'),
-    ('892422cd-0835-479e-84de-9265d3f2dc6a', 'JavaScript for Beginner', 'Js for New commer', 'Marx Plank', 'javaScript for beginner', '/images/ZeroToOne.jpg', 29.99, 'a1b9b31d-e73c-4112-af7c-b68530f38222');
+    ('892422cd-0835-479e-84de-9265d3f2dc6a', 'JavaScript for Beginner', 'Js for New commer', 'Marx Plank', 'javascript for beginner', '/images/ZeroToOne.jpg', 29.99, 'a1b9b31d-e73c-4112-af7c-b68530f38222');
